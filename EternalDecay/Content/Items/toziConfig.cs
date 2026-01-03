07debb5611fd79f29a564ecedd91d22011f8d84b
Liukeyu <liukele015@gmail.com>
2026-01-03 15:10:16 +0800

    添加 ToziConfig 类实现 IEntityConfig 接口
    新增类 `ToziConfig`，实现了 `IEntityConfig` 接口。该类包含方法 `GetDlcIds`、`CreatePrefab`、`OnPrefabInit` 和 `OnSpawn`。`CreatePrefab` 方法用于创建并返回一个新的游戏对象，设置其动画和属性，其他方法目前为空实现。

---
+++ EternalDecay/Content/Items/toziConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Klei.AI;
+using UnityEngine;
+
+namespace EternalDecay.Content.Items
+{
+    public class ToziConfig : IEntityConfig
+    {
+        public string[] GetDlcIds()
+        {
+            return null;
+        }
+
+        public GameObject CreatePrefab()
+        {
+            GameObject gameObject = EntityTemplates.CreateLooseEntity(
+                "Ktozi",
+                Configs.STRINGS.ITEMS.INDUSTRIAL_PRODUCTS.TOZI.NAME,
+                Configs.STRINGS.ITEMS.INDUSTRIAL_PRODUCTS.TOZI.DESC,
+                5f,
+                true,
+                Assets.GetAnim("tozi_kanim"),
+                "object",
+                Grid.SceneLayer.SolidConduits,
+                EntityTemplates.CollisionShape.RECTANGLE,
+                0.6f,
+                0.6f,
+                true, 
+                0,
+                SimHashes.Creature,
+                new List<Tag> { GameTags.IndustrialIngredient }
+            );
+
+            KBatchedAnimController animController = gameObject.GetComponent<KBatchedAnimController>();
+            if (animController != null)
+            {
+                animController.animScale = 0.0015f;
+            }
+
+
+            return gameObject;
+        }
+
+        public void OnPrefabInit(GameObject inst)
+        {
+
+        }
+
+        public void OnSpawn(GameObject inst)
+        {
+
+
+        }
+
+
+
+
+    }
+}
