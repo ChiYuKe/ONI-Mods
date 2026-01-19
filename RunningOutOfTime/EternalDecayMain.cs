81f39d4f5905133f39992f5025ce39cbceecfe71
Liukeyu <liukele015@gmail.com>
2026-01-19 20:33:56 +0800

    添加EternalDecayMain类及KModPatch补丁
    在 `EternalDecayMain.cs` 中新增 `EternalDecayMain` 类，包含静态字段 `MinionAgeThreshold` 和 `NotifyDeathApplied` 方法，用于处理游戏对象死亡通知。
    
    在 `KModPatch.cs` 中添加 `KModPatch` 类及其内部类 `Patch`，重写 `OnLoad` 方法，并新增两个补丁类以支持本地化和自定义效果注册。

---
+++ RunningOutOfTime/EternalDecayMain.cs
@@ -0,0 +1,25 @@
+﻿using RunningOutOfTime.Content.Config;
+using UnityEngine;
+
+namespace RunningOutOfTime
+{
+    internal class EternalDecayMain : KMonoBehaviour
+    {
+        // 这里的配置字段保持不变
+        public static float MinionAgeThreshold = TUNINGS.AGE.MINION_AGE_THRESHOLD;
+
+        // 通知方法保持静态，方便组件调用
+        public static void NotifyDeathApplied(GameObject gameObject)
+        {
+            Notifier notifier = gameObject.AddOrGet<Notifier>();
+            Notification notification = new Notification(
+                Content.Config.STRINGS.MISC.NOTIFICATIONS.DEATHROULETTE.NAME,
+                NotificationType.BadMinor,
+                (notificationList, data) => notificationList.ReduceMessages(false),
+                "/t• " + gameObject.GetProperName(),
+                true
+            );
+            notifier.Add(notification, "");
+        }
+    }
+}
---
+++ RunningOutOfTime/KModPatch.cs
@@ -0,0 +1,46 @@
+﻿using CykUtils;
+using HarmonyLib;
+using KMod;
+using Microsoft.Win32;
+using RunningOutOfTime.Content.Core;
+
+
+namespace RunningOutOfTime
+{
+    public class KModPatch
+    {
+        public class Patch : UserMod2
+        {
+            public override void OnLoad(Harmony harmony)
+            {
+                base.OnLoad(harmony);
+
+            }
+        }
+        // 本地化补丁，初始化时注册并加载翻译字符串。//
+        [HarmonyPatch(typeof(Localization), "Initialize")]
+
+        private class Translate_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                Loc.Translate(typeof(Content.Config.STRINGS), true);
+            }
+        }
+
+
+        // 在 ModifierSet 初始化时注册所有自定义效果。//
+        [HarmonyPatch(typeof(ModifierSet), "Initialize")]
+        public static class ModifierSet_Initialize_Patch
+        {
+            public static void Postfix(ModifierSet __instance)
+            {
+                ModEffects.RegisterAll(__instance);
+            }
+        }
+
+
+
+
+    }
+}
