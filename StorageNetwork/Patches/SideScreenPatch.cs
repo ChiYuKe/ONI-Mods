9fc17e3d2d2e6e1cc4fedc97fd85bb2479501c45
Liukeyu <liukele015@gmail.com>
2026-06-08 01:12:47 +0800

    refactor(StorageNetwork): move side screen UI installation out of patches

--- StorageNetwork/Patches/SideScreenPatch.cs
+++ StorageNetwork/Patches/SideScreenPatch.cs
@@ -1,11 +1,7 @@
-using System.Collections.Generic;
 using HarmonyLib;
 using StorageNetwork.Components;
-using StorageNetwork.Core;
-using StorageNetwork.UI;
+using StorageNetwork.UI.Installers;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Linq;
 
 namespace StorageNetwork.Patches
 {
@@ -23,8 +19,7 @@ namespace StorageNetwork.Patches
 
                 try
                 {
-                    StorageNetworkWorldTextPanel.EnsureInstalled(__instance.gameObject);
-                    StorageNetworkManagementButton.Add(__instance);
+                    StorageNetworkManagementMenuInstaller.Install(__instance);
                 }
                 catch (System.Exception exception)
                 {
@@ -64,224 +59,12 @@ namespace StorageNetwork.Patches
 
                 try
                 {
-                    StorageNetworkCoreSideScreenRegistration.Add(__instance);
+                    StorageNetworkCoreSideScreenInstaller.Install(__instance);
                 }
                 catch (System.Exception exception)
                 {
                     Debug.LogWarning("[StorageNetwork] Failed to add core side screen: " + exception);
                 }
-            }
-        }
-
-        private static class StorageNetworkManagementButton
-        {
-            private const string ButtonName = "StorageNetworkManagementButton";
-            private const string IconName = "storage_network_overlay";
-
-            public static void Add(ManagementMenu menu)
-            {
-                Transform parent = ResolveToggleParent(menu);
-                if (parent == null)
-                {
-                    return;
-                }
-
-                Transform existing = parent.Find(ButtonName);
-                if (existing != null)
-                {
-                    return;
-                }
-
-                KToggle template = ResolveTemplate(menu);
-                if (template == null)
-                {
-                    return;
-                }
-
-                GameObject buttonObject = Object.Instantiate(template.gameObject, parent, false);
-                buttonObject.name = ButtonName;
-                buttonObject.SetActive(true);
-
-                KToggle button = buttonObject.GetComponent<KToggle>();
-                button.ClearOnClick();
-                button.group = null;
-                button.isOn = false;
-                button.interactable = true;
-
-                ImageToggleState toggleState = buttonObject.GetComponent<ImageToggleState>();
-                toggleState?.SetInactive();
-
-                LocText label = buttonObject.GetComponentInChildren<LocText>(true);
-                if (label != null)
-                {
-                    SetButtonLabel(label);
-                }
-
-                if (button.fgImage != null)
-                {
-                    Sprite sprite = StorageNetworkSpriteLoader.GetSprite(IconName);
-                    if (sprite != null)
-                    {
-                        button.fgImage.sprite = sprite;
-                        button.fgImage.color = Color.white;
-                    }
-                }
-
-                HierarchyReferences references = buttonObject.GetComponent<HierarchyReferences>();
-                if (references != null)
-                {
-                    DisableIfPresent(references, "ResearchIcon");
-                    DisableIfPresent(references, "AlertImage");
-                    DisableIfPresent(references, "GlowImage");
-                    DisableIfPresent(references, "CheckMark");
-                    DisableIfPresent(references, "Checkmark");
-                    DisableIfPresent(references, "Notification");
-                    DisableIfPresent(references, "TopRightIcon");
-                }
-
-                ToolTip toolTip = button.GetComponent<ToolTip>() ?? button.gameObject.AddComponent<ToolTip>();
-                toolTip.SetSimpleTooltip(STRINGS.UI.STORAGE_NETWORK.OVERVIEW_TOOLTIP);
-                button.onClick += () =>
-                {
-                    button.isOn = false;
-                    toggleState?.SetInactive();
-                    KMonoBehaviour.PlaySound(GlobalAssets.GetSound("HUD_Click", false));
-                    StorageNetworkPanel.Show();
-                };
-
-                button.transform.SetSiblingIndex(GetInsertIndex(parent));
-            }
-
-            private static void SetButtonLabel(LocText label)
-            {
-                string title = STRINGS.Get(STRINGS.UI.STORAGE_NETWORK.TITLE);
-                label.key = string.Empty;
-                label.enableAutoSizing = false;
-                label.fontSize = Mathf.Min(label.fontSize, 15f);
-                label.overflowMode = TMPro.TextOverflowModes.Ellipsis;
-                label.textWrappingMode = TMPro.TextWrappingModes.NoWrap;
-                label.SetText(title);
-                label.text = title;
-
-                RectTransform labelRect = label.rectTransform();
-                if (labelRect != null)
-                {
-                    labelRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 96f);
-                }
-
-                RectTransform buttonRect = label.GetComponentInParent<KToggle>()?.GetComponent<RectTransform>();
-                if (buttonRect != null)
-                {
-                    buttonRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Mathf.Max(buttonRect.rect.width, 112f));
-                }
-            }
-
-            private static Transform ResolveToggleParent(ManagementMenu menu)
-            {
-                Traverse traverse = Traverse.Create(menu);
-                Transform toggleParent = traverse.Field("toggleParent").GetValue<Transform>();
-                return toggleParent != null ? toggleParent : menu.transform;
-            }
-
-            private static KToggle ResolveTemplate(ManagementMenu menu)
-            {
-                Traverse traverse = Traverse.Create(menu);
-                List<KToggle> toggles = traverse.Field("toggles").GetValue<List<KToggle>>();
-                KToggle liveTemplate = toggles != null
-                    ? toggles.FirstOrDefault(toggle =>
-                        toggle != null &&
-                        toggle.gameObject != null &&
-                        toggle.gameObject.activeSelf &&
-                        IsPrimaryManagementButton(toggle))
-                    : null;
-                if (liveTemplate != null)
-                {
-                    return liveTemplate;
-                }
-
-                KToggle template = traverse.Field("researchButtonPrefab").GetValue<KToggle>();
-                if (template != null)
-                {
-                    return template;
-                }
-
-                template = traverse.Field("smallPrefab").GetValue<KToggle>();
-                if (template != null)
-                {
-                    return template;
-                }
-
-                return toggles != null ? toggles.FirstOrDefault(toggle => toggle != null) : null;
-            }
-
-            private static int GetInsertIndex(Transform parent)
-            {
-                int starMapIndex = parent.childCount;
-                for (int i = 0; i < parent.childCount; i++)
-                {
-                    string name = parent.GetChild(i).name;
-                    if (name.Contains(STRINGS.Get(STRINGS.UI.STORAGE_NETWORK.STARMAP_NAME_HINT)) || name.Contains("STARMAP"))
-                    {
-                        starMapIndex = i;
-                        break;
-                    }
-                }
-
-                return Mathf.Clamp(starMapIndex, 0, parent.childCount);
-            }
-
-            private static void DisableIfPresent(HierarchyReferences references, string key)
-            {
-                Component component = references.GetReference(key);
-                if (component != null)
-                {
-                    component.gameObject.SetActive(false);
-                }
-            }
-
-            private static bool IsPrimaryManagementButton(KToggle toggle)
-            {
-                LocText label = toggle.GetComponentInChildren<LocText>(true);
-                if (label == null)
-                {
-                    return false;
-                }
-
-                string text = label.text;
-                return text == global::STRINGS.UI.VITALS ||
-                       text == global::STRINGS.UI.CONSUMABLES ||
-                       text == global::STRINGS.UI.JOBS ||
-                       text == global::STRINGS.UI.SCHEDULE ||
-                       text == global::STRINGS.UI.SKILLS;
-            }
-        }
-
-        private static class StorageNetworkCoreSideScreenRegistration
-        {
-            private const string ScreenName = "StorageNetworkCoreSideScreen";
-
-            public static void Add(DetailsScreen detailsScreen)
-            {
-                HarmonyLib.Traverse traverse = HarmonyLib.Traverse.Create(detailsScreen);
-                List<DetailsScreen.SideScreenRef> sideScreens = traverse.Field("sideScreens").GetValue<List<DetailsScreen.SideScreenRef>>();
-                GameObject contentBody = traverse.Field("sideScreenContentBody").GetValue<GameObject>();
-                if (sideScreens == null || contentBody == null || sideScreens.Any(screen => screen.name == ScreenName))
-                {
-                    return;
-                }
-
-                GameObject prefabObject = new GameObject(ScreenName);
-                prefabObject.transform.SetParent(contentBody.transform, false);
-                prefabObject.SetActive(false);
-
-                StorageNetworkCoreSideScreen sideScreen = prefabObject.AddComponent<StorageNetworkCoreSideScreen>();
-                sideScreens.Add(new DetailsScreen.SideScreenRef
-                {
-                    name = ScreenName,
-                    offset = Vector2.zero,
-                    screenPrefab = sideScreen,
-                    tab = DetailsScreen.SidescreenTabTypes.Config
-                });
             }
         }
     }
---
+++ StorageNetwork/UI/Installers/StorageNetworkManagementMenuInstaller.cs
@@ -0,0 +1,203 @@
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using StorageNetwork.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StorageNetwork.UI.Installers
+{
+    internal static class StorageNetworkManagementMenuInstaller
+    {
+        private const string ButtonName = "StorageNetworkManagementButton";
+        private const string IconName = "storage_network_overlay";
+
+        public static void Install(ManagementMenu menu)
+        {
+            if (menu == null)
+            {
+                return;
+            }
+
+            StorageNetworkWorldTextPanel.EnsureInstalled(menu.gameObject);
+            AddManagementButton(menu);
+        }
+
+        private static void AddManagementButton(ManagementMenu menu)
+        {
+            Transform parent = ResolveToggleParent(menu);
+            if (parent == null)
+            {
+                return;
+            }
+
+            Transform existing = parent.Find(ButtonName);
+            if (existing != null)
+            {
+                return;
+            }
+
+            KToggle template = ResolveTemplate(menu);
+            if (template == null)
+            {
+                return;
+            }
+
+            GameObject buttonObject = Object.Instantiate(template.gameObject, parent, false);
+            buttonObject.name = ButtonName;
+            buttonObject.SetActive(true);
+
+            KToggle button = buttonObject.GetComponent<KToggle>();
+            button.ClearOnClick();
+            button.group = null;
+            button.isOn = false;
+            button.interactable = true;
+
+            ImageToggleState toggleState = buttonObject.GetComponent<ImageToggleState>();
+            toggleState?.SetInactive();
+
+            LocText label = buttonObject.GetComponentInChildren<LocText>(true);
+            if (label != null)
+            {
+                SetButtonLabel(label);
+            }
+
+            if (button.fgImage != null)
+            {
+                Sprite sprite = StorageNetworkSpriteLoader.GetSprite(IconName);
+                if (sprite != null)
+                {
+                    button.fgImage.sprite = sprite;
+                    button.fgImage.color = Color.white;
+                }
+            }
+
+            HierarchyReferences references = buttonObject.GetComponent<HierarchyReferences>();
+            if (references != null)
+            {
+                DisableIfPresent(references, "ResearchIcon");
+                DisableIfPresent(references, "AlertImage");
+                DisableIfPresent(references, "GlowImage");
+                DisableIfPresent(references, "CheckMark");
+                DisableIfPresent(references, "Checkmark");
+                DisableIfPresent(references, "Notification");
+                DisableIfPresent(references, "TopRightIcon");
+            }
+
+            ToolTip toolTip = button.GetComponent<ToolTip>() ?? button.gameObject.AddComponent<ToolTip>();
+            toolTip.SetSimpleTooltip(STRINGS.UI.STORAGE_NETWORK.OVERVIEW_TOOLTIP);
+            button.onClick += () =>
+            {
+                button.isOn = false;
+                toggleState?.SetInactive();
+                KMonoBehaviour.PlaySound(GlobalAssets.GetSound("HUD_Click", false));
+                StorageNetworkPanel.Show();
+            };
+
+            button.transform.SetSiblingIndex(GetInsertIndex(parent));
+        }
+
+        private static void SetButtonLabel(LocText label)
+        {
+            string title = STRINGS.Get(STRINGS.UI.STORAGE_NETWORK.TITLE);
+            label.key = string.Empty;
+            label.enableAutoSizing = false;
+            label.fontSize = Mathf.Min(label.fontSize, 15f);
+            label.overflowMode = TMPro.TextOverflowModes.Ellipsis;
+            label.textWrappingMode = TMPro.TextWrappingModes.NoWrap;
+            label.SetText(title);
+            label.text = title;
+
+            RectTransform labelRect = label.rectTransform();
+            if (labelRect != null)
+            {
+                labelRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 96f);
+            }
+
+            RectTransform buttonRect = label.GetComponentInParent<KToggle>()?.GetComponent<RectTransform>();
+            if (buttonRect != null)
+            {
+                buttonRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Mathf.Max(buttonRect.rect.width, 112f));
+            }
+        }
+
+        private static Transform ResolveToggleParent(ManagementMenu menu)
+        {
+            Traverse traverse = Traverse.Create(menu);
+            Transform toggleParent = traverse.Field("toggleParent").GetValue<Transform>();
+            return toggleParent != null ? toggleParent : menu.transform;
+        }
+
+        private static KToggle ResolveTemplate(ManagementMenu menu)
+        {
+            Traverse traverse = Traverse.Create(menu);
+            List<KToggle> toggles = traverse.Field("toggles").GetValue<List<KToggle>>();
+            KToggle liveTemplate = toggles != null
+                ? toggles.FirstOrDefault(toggle =>
+                    toggle != null &&
+                    toggle.gameObject != null &&
+                    toggle.gameObject.activeSelf &&
+                    IsPrimaryManagementButton(toggle))
+                : null;
+            if (liveTemplate != null)
+            {
+                return liveTemplate;
+            }
+
+            KToggle template = traverse.Field("researchButtonPrefab").GetValue<KToggle>();
+            if (template != null)
+            {
+                return template;
+            }
+
+            template = traverse.Field("smallPrefab").GetValue<KToggle>();
+            if (template != null)
+            {
+                return template;
+            }
+
+            return toggles != null ? toggles.FirstOrDefault(toggle => toggle != null) : null;
+        }
+
+        private static int GetInsertIndex(Transform parent)
+        {
+            int starMapIndex = parent.childCount;
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                string name = parent.GetChild(i).name;
+                if (name.Contains(STRINGS.Get(STRINGS.UI.STORAGE_NETWORK.STARMAP_NAME_HINT)) || name.Contains("STARMAP"))
+                {
+                    starMapIndex = i;
+                    break;
+                }
+            }
+
+            return Mathf.Clamp(starMapIndex, 0, parent.childCount);
+        }
+
+        private static void DisableIfPresent(HierarchyReferences references, string key)
+        {
+            Component component = references.GetReference(key);
+            if (component != null)
+            {
+                component.gameObject.SetActive(false);
+            }
+        }
+
+        private static bool IsPrimaryManagementButton(KToggle toggle)
+        {
+            LocText label = toggle.GetComponentInChildren<LocText>(true);
+            if (label == null)
+            {
+                return false;
+            }
+
+            string text = label.text;
+            return text == global::STRINGS.UI.VITALS ||
+                   text == global::STRINGS.UI.CONSUMABLES ||
+                   text == global::STRINGS.UI.JOBS ||
+                   text == global::STRINGS.UI.SCHEDULE ||
+                   text == global::STRINGS.UI.SKILLS;
+        }
+    }
+}
